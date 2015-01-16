af3675b51e8930e8d83dbc56f304357c75898f88
santisuk <istudents@iClassrooms-iMac-40.local>
2015-01-16 16:42:52 +0700

    test3_if statement
    if staementlab 1-2

---
+++ test3_If satatement.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+if(onSaleInferred){
+    println("\(nameInferred) on sale for \(priceInferred)!")
+}else{
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
