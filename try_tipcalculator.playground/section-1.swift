9ea7479df67071db97d38ce1f14b0c1f8d3088af
santisuk <istudents@iClassrooms-iMac-40.local>
2015-01-23 13:55:38 +0700

    tipcalculator
    try_tipcalcullator

---
+++ try_tipcalculator.playground/section-1.swift
@@ -0,0 +1,43 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class TipCalculator {
+    //2
+    let total : Double
+    let taxPct : Double
+    let Subtotal : Double
+    
+    //3
+    init(total:Double , taxPct:Double){
+        self.total = total
+        self.taxPct = taxPct
+        Subtotal = total/(taxPct+1)
+    }
+    
+    //4
+    func calcTipWithTipPct(tipPct:Double)->Double{
+        return Subtotal*tipPct
+    }
+    
+    //1
+    func returnPossibleTips()->[Int : Double]{
+        let possibleTipsInferred = [0.15,0.18,0.20]
+        let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
+        var numberOfItems = possibleTipsInferred.count
+        
+        //2
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPCt = Int(possibleTip*100)
+            //3
+            retval[intPCt] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
+//6
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.returnPossibleTips()
