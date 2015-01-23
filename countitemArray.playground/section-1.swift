a0bccf2177e054e17e3400f21537bf27f9c0c1fb
santisuk <istudents@iClassrooms-iMac-40.local>
2015-01-23 14:06:09 +0700

    countItem
    CountitemArray

---
+++ countitemArray.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let possibletipsInFerred = [0.15,0.18,0.20]
+let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
+var numberOfItems = possibletipsInFerred.count
