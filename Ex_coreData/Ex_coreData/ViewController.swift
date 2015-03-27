57c2d24dab6562d3a886bf989e54df4f255020cc
54011212151may <istudents@iClassrooms-iMac-2.local>
2015-03-27 17:37:29 +0700

    UIKit Animation_V2
    UIKit Animationv2

---
+++ Ex_coreData/Ex_coreData/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Ex_coreData
+//
+//  Created by iStudents on 3/20/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBAction func addItem(sender: AnyObject) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
